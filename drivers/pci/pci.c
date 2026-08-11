dfbb1118f59a011c0401b84b956fa1175fc17ebb
Surabhi Chythanya Kumar <schythanyaku@nvidia.com>
2026-08-10 17:33:39 -0700

    PCI: Export pcie_retrain_link for module use
    Allow platform drivers built as modules to request a PCIe link
    retrain after bringing a link back up. Needed by the Tegra254
    DGX Spark hotplug driver after cable reinsertion.
    
    Signed-off-by: Vaibhav Vyas <vavyas@nvidia.com>
    Signed-off-by: Scott Fudally <sfudally@nvidia.com>
    Signed-off-by: Surabhi Chythanya Kumar <schythanyaku@nvidia.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4617,6 +4617,7 @@ int pcie_retrain_link(struct pci_dev *pdev, bool use_lt)
 
 	return rc;
 }
+EXPORT_SYMBOL_GPL(pcie_retrain_link);
 
 /**
  * pcie_wait_for_link_delay - Wait until link is active or inactive
